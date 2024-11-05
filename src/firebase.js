8370dd30a1bca13e6a8ebba74a980bbc88fac1da
Munim-Ali <munimali52@gmail.com>
2024-11-05 17:00:47 +0500

    Firebase analytics env

--- src/firebase.js
+++ src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBSH4xPaW4p6jOA0NNSm-WDvTOIheUl4q4",
-  authDomain: "munim-ali.firebaseapp.com",
-  projectId: "munim-ali",
-  storageBucket: "munim-ali.firebasestorage.app",
-  messagingSenderId: "564172549438",
-  appId: "1:564172549438:web:f9c6afd77f333319e6d06f",
-  measurementId: "G-PMVHK9Z4X4",
+  apiKey: process.env.REACT_APP_FIREBASE_apiKey,
+  authDomain: process.env.REACT_APP_FIREBASE_authDomain,
+  projectId: process.env.REACT_APP_FIREBASE_projectId,
+  storageBucket: process.env.REACT_APP_FIREBASE_storageBucket,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_messagingSenderId,
+  appId: process.env.REACT_APP_FIREBASE_appId,
+  measurementId: process.env.REACT_APP_FIREBASE_measurementId,
 };
 
 // Initialize Firebase
