2d870e1f91dee10415df7edf764a81118a292ac8
Munim-Ali <munimali52@gmail.com>
2024-11-05 19:06:35 +0500

    contact form functional

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
-import { RouterProvider } from 'react-router'
-import { router } from './route/router'
+import React from "react";
+import { RouterProvider } from "react-router";
+import { router } from "./route/router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const App = () => {
   return (
-    <RouterProvider router={router} />
-  )
-}
+    <>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </>
+  );
+};
 
-export default App
+export default App;
--- src/components/sections/contact/contactForm.jsx
+++ src/components/sections/contact/contactForm.jsx
@@ -1,51 +1,106 @@
-import React from 'react'
-import { RiMailLine } from '@remixicon/react'
-import SlideUp from '../../../utlits/animations/slideUp'
+import React, { useRef } from "react";
+import { RiMailLine } from "@remixicon/react";
+import emailjs from "@emailjs/browser";
+import SlideUp from "../../../utlits/animations/slideUp";
+import { toast } from "react-toastify";
 
 const ContactForm = () => {
-    return (
-        <div className="col-lg-8">
-            <SlideUp>
-                <div className="contact-form contact-form-area">
-                    <form id="contactForm" className="contactForm" name="contactForm" action="assets/php/form-process.php" method="post">
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label for="name">Full Name</label>
-                                    <input type="text" id="name" name="name" className="form-control" value="" placeholder="Steve Milner" required="" data-error="Please enter your Name" />
-                                    <label for="name" className="for-icon"><i className="far fa-user"></i></label>
-                                    <div className="help-block with-errors"></div>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <label for="email">Email Address</label>
-                                    <input type="email" id="email" name="email" className="form-control" value="" placeholder="hello@websitename.com" required="" data-error="Please enter your Email" />
-                                    <label for="email" className="for-icon"><i className="far fa-envelope"></i></label>
-                                    <div className="help-block with-errors"></div>
-                                </div>
-                            </div>
-                            <div className="col-md-12">
-                                <div className="form-group">
-                                    <label for="message">Your Message</label>
-                                    <textarea name="message" id="message" className="form-control" rows="4" placeholder="Write Your message" required="" data-error="Please Write your Message"></textarea>
-                                    <div className="help-block with-errors"></div>
-                                </div>
-                            </div>
-                            <div className="col-md-12">
-                                <div className="form-group mb-0">
-                                    <button type="submit" className="theme-btn">
-                                        Send Me Message <i><RiMailLine size={15} /></i>
-                                    </button>
-                                    <div id="msgSubmit" className="hidden"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </SlideUp>
-        </div>
-    )
-}
+  const form = useRef();
 
-export default ContactForm
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_lpelvnv", "template_2bd4fgu", form.current, {
+        publicKey: "RxDLWzLCJ9SzQUns5",
+      })
+      .then(
+        () => {
+          toast.success("Form submitted successfully!");
+          reset();
+        },
+        (error) => {
+          toast.error("Failed to send, Please contact via email");
+        }
+      );
+  };
+  return (
+    <div className="col-lg-8">
+      <SlideUp>
+        <div className="contact-form contact-form-area">
+          <form
+            id="contactForm"
+            className="contactForm"
+            ref={form}
+            onSubmit={sendEmail}
+          >
+            <div className="row">
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label for="name">Full Name</label>
+                  <input
+                    type="text"
+                    name="name"
+                    className="form-control"
+                    placeholder="Steve Milner"
+                    required
+                    data-error="Please enter your Name"
+                  />
+                  <label for="name" className="for-icon">
+                    <i className="far fa-user"></i>
+                  </label>
+                  <div className="help-block with-errors"></div>
+                </div>
+              </div>
+              <div className="col-md-6">
+                <div className="form-group">
+                  <label for="email">Email Address</label>
+                  <input
+                    type="email"
+                    name="email"
+                    className="form-control"
+                    placeholder="hello@websitename.com"
+                    required
+                    data-error="Please enter your Email"
+                  />
+                  <label for="email" className="for-icon">
+                    <i className="far fa-envelope"></i>
+                  </label>
+                  <div className="help-block with-errors"></div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="form-group">
+                  <label for="message">Your Message</label>
+                  <textarea
+                    name="message"
+                    id="message"
+                    className="form-control"
+                    rows="4"
+                    placeholder="Write Your message"
+                    required
+                    data-error="Please Write your Message"
+                  ></textarea>
+                  <div className="help-block with-errors"></div>
+                </div>
+              </div>
+              <div className="col-md-12">
+                <div className="form-group mb-0">
+                  <button type="submit" className="theme-btn">
+                    Send Me Message{" "}
+                    <i>
+                      <RiMailLine size={15} />
+                    </i>
+                  </button>
+                  <div id="msgSubmit" className="hidden"></div>
+                </div>
+              </div>
+            </div>
+          </form>
+        </div>
+      </SlideUp>
+    </div>
+  );
+};
+
+export default ContactForm;
